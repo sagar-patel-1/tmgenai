b38fcc7475b353ceb933649d6aa89af208ddb3ce
amanas9 <aman.kr.phe16@itbhu.ac.in>
2023-06-30 02:57:22 +0530

    initial commit

---
+++ src/test/java/com/genai/tmgenai/TmGenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genai.tmgenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmGenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
